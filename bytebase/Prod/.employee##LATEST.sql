76d7bda695314b72d2518e21e06be35fa8e82170
Candy <104962472+Candybase@users.noreply.github.com>
2023-01-12 22:31:54 +0800

    [Bytebase] Update latest schema for "employee" after migration 202301120002
    THIS COMMIT IS AUTO-GENERATED BY BYTEBASE
    
    https://29dd-209-141-51-123.eu.ngrok.io/issue/employee-alter-schema-104?stage=104
    
    --------Original migration change--------
    
    https://github.com/Candybase/demo/commit/8ee0f746e42a297d59171e96c8484841d5d6aba4
    
    Create employee##202301120002##ddl##alter_table_sample.sql

--- bytebase/Prod/.employee##LATEST.sql
+++ bytebase/Prod/.employee##LATEST.sql
@@ -174,6 +174,7 @@ CREATE TABLE `session` (
 CREATE TABLE `tb_smaple` (
   `id` int NOT NULL,
   `status` tinyint(1) DEFAULT NULL,
+  `phone` varchar(16) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
